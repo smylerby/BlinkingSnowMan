fda96fd8e0f161eab0a9541ba3da88bdb8d9bebd
Rustam Shorov <srmparusa@gmail.com>
2018-12-04 11:02:11 +0300

    Initial Commit

---
+++ BlinkingSnowMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BlinkingSnowMan
+//
+//  Created by Rustam Shorov on 04.12.2018.
+//  Copyright © 2018 Rustam Shorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
